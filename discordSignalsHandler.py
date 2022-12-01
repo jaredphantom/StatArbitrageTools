de934419cb029cfef49f050a9cd0e93b0e808ff0
Jared Schoel <78913072+jaredphantom@users.noreply.github.com>
2022-12-01 22:59:31 +0000

    Update discordSignalsHandler.py

--- discordSignalsHandler.py
+++ discordSignalsHandler.py
@@ -1,10 +1,15 @@
-from subprocess import call
+from subprocess import Popen
 import time
+from datetime import datetime as dt
 
 minute = 60
 hour = minute * 60
 day = hour * 24
 
 while True:
-    call(["python", "discordSignals.py"])
-    time.sleep(day)
+    if dt.today().hour == 20:
+        while True:
+            proc = Popen(["python", "discordSignals.py"])
+            time.sleep(day)
+            proc.wait()
+    time.sleep(minute * 10)
